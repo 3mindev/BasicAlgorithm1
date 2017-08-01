d904587194309504a1cc1d7036d83779733a7aef
gichanlee <gichan.lee@3mindev.club>
2017-08-01 16:45:22 +0900

    11

--- BasicAlgorithm/HelloWorld.c
+++ BasicAlgorithm/HelloWorld.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-	int int1, int2, int3, temp;
-	scanf("%d %d %d", &int1, &int2, &int3);
+	float img_x, img_y, copy_x, copy_y, answer1, answer2, answer3, answer4, temp;
+	scanf("%f %f %f %f", &img_x, &img_y, &copy_x, &copy_y);
 
-	if (int1 > int2) {
-		temp = int2;
-		int2 = int1;
-		int1 = temp;
-	}
-	if (int1 > int3) {
-		temp = int3;
-		int3 = int1;
-		int1 = temp;
-	}
-	if (int2 > int3) {
-		temp = int3;
-		int3 = int2;
-		int2 = temp;
-	}
+	answer1 = -1.0;
+	answer2 = -1.0;
+	answer3 = -1.0;
+	answer4 = -1.0;
 
-	if (pow(int1, 2.0) + pow(int2, 2.0) == pow(int3, 2.0)) {
-		printf("right");
-	}
-	else if (pow(int1, 2.0) + pow(int2, 2.0) > pow(int3, 2.0)){
-		printf("acute");
-	}
-	else if (pow(int1, 2.0) + pow(int2, 2.0) < pow(int3, 2.0) {
-		printf("obtuse");
-	}
+	if (img_y * copy_x / img_x < copy_y) { answer1 = copy_x / img_x*100; }
+	if (img_y * copy_y / img_x < copy_x) { answer2 = copy_y / img_x*100; }
+	if (img_x * copy_x / img_y < copy_y) { answer3 = copy_x / img_y*100; }
+	if (img_x * copy_y / img_y < copy_x) { answer4 = copy_y / img_y*100; }
 
+	//printf("%.2f %.2f %.2f %.2f\n", answer1, answer2, answer3, answer4);
+	if ( answer1 > answer2 ){
+		temp = answer2;
+		answer2 = answer1;
+		answer1 = temp;
+	}
+	if (answer1 > answer3) {
+		temp = answer3;
+		answer3 = answer1;
+		answer1 = temp;
+	}
+	if (answer1 > answer4) {
+		temp = answer4;
+		answer4 = answer1;
+		answer1 = temp;
+	}
+	if (answer2 > answer3) {
+		temp = answer3;
+		answer3 = answer2;
+		answer2 = temp;
+	}
+	if (answer2 > answer4) {
+		temp = answer4;
+		answer4 = answer2;
+		answer2 = temp;
+	}
+	if (answer3 > answer4) {
+		temp = answer4;
+		answer4 = answer3;
+		answer3 = temp;
+	}
+	printf("%.2f%", answer4);
+
+	scanf("%d", &answer4);
 	return 0;
 
 }
